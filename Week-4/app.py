5d4e87b4aebb0adffc3374376ba5a95a9e0e0f16
Hao-Yu Yang <69504605+a05031113@users.noreply.github.com>
2022-10-11 00:48:20 +0800

    Add files via upload

---
+++ Week-4/app.py
@@ -0,0 +1,74 @@
+# import tools I need
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import render_template
+from flask import session
+
+app = Flask(__name__)
+
+# index page
+@app.route("/", methods=["GET", "POST"])
+def index(): 
+    # if already login, return to /member
+    if session.get("userID"):
+        return redirect("/member")
+    # if not login, go to index 
+    else:
+        return render_template("index.html")
+
+# check signin data
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    # if "post"
+    if request.method == "POST":
+        # user data 
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # if username of password is blank
+        if not username or not password:
+            return redirect("/error?messange=請輸入帳號、密碼")
+        # if username and password is test
+        elif username=="test" and password=="test":
+            session["userID"]=username
+            session["userPass"]=password
+            return redirect("/member")
+        # if one of it is wrong
+        else:
+            return redirect("/error?messange=帳號或密碼輸入錯誤")
+    else:
+        return render_template("index.html")
+
+# signout and clean session and return to index
+@app.route("/signout")
+def signout():
+    session.clear()
+    return redirect("/")
+
+# member page
+@app.route("/member", methods=["GET", "POST"])
+def member():
+    # if already login
+    if session.get("userID"):
+        return render_template("member.html")
+    # if not login yet, need to login first
+    else:
+        return redirect("/")
+
+# if something wrong
+@app.route("/error")
+def error():
+    # show error messange
+    messange = request.args.get("messange")
+    return render_template("error.html", messange=messange)
+
+# calculate square of number
+@app.route("/square")
+def square():
+    num = int(request.args.get("number"))
+    square=num**2
+    return render_template("square.html", square=square)
+
+if __name__=="__main__":
+    app.secret_key="secret"
+    app.run(port=3000, debug=True)
