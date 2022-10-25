f5ec1811f2dd2a35455de3223252d61c89cf88c6
羊 <yanghaoyu@yanghaoyudeMacBook-Pro.local>
2022-10-25 17:49:28 +0800

    clean the code
    remove method and something more

--- Week-6/app.py
+++ Week-6/app.py
@@ -5,7 +5,6 @@ from flask import redirect
 from flask import render_template
 from flask import session
 import mysql.connector as connector
-from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 
 db = connector.connect(
@@ -20,7 +19,7 @@ app = Flask(__name__)
 app.secret_key="secret"
 
 # index page
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index(): 
     # if already login, return to /member
     if session.get("userID"):
@@ -29,68 +28,63 @@ def index():
     else:
         return render_template("index.html")
 
-@app.route("/signup", methods=["GET", "POST"])
+@app.route("/signup", methods=["POST"])
 def signup():
     name = request.form.get("name")
     username = request.form.get("username")
     password = request.form.get("password")
     confirmation = request.form.get("confirmation")
-    if request.method == "POST":
-        # check fill in information or not
-        if not name:
-            return redirect("/error?message=請輸入姓名")
-        elif not username or not password:
-            return redirect("/error?message=請輸入帳號, 密碼")
-        elif not password == confirmation:
-            return redirect("/error?message=密碼不一致")
-        # collect data form member
-        checkUser = "SELECT username FROM member WHERE username =%s"
-        User = (username,)
-        cursor.execute(checkUser, User)
-        checklist = []
-        for row in cursor:
-            checklist.append(row)
-        if len(checklist) == 1:
-            return redirect("error?message=帳號已經被註冊")
-        else:
-            # insert data into database
-            now = datetime.now()
-            date_now = now.strftime("%y-%m-%d %H:%M:%S")
-            insert = "INSERT INTO member (name, username, password, time) VALUES (%s, %s, %s, %s)"
-            insertVal = (name, username, generate_password_hash(password), date_now)
-            cursor.execute(insert, insertVal)
-            db.commit()
-            # save username into session
-            session["userID"]=username
-            # return to /member
-            return redirect("/member")
+    # check fill in information or not
+    if not name:
+        return redirect("/error?message=請輸入姓名")
+    elif not username or not password:
+        return redirect("/error?message=請輸入帳號, 密碼")
+    elif not password == confirmation:
+        return redirect("/error?message=密碼不一致")
+    # check already signup or not
+    checkUser = "SELECT username FROM member WHERE username =%s"
+    User = (username,)
+    cursor.execute(checkUser, User)
+    checklist = []
+    for row in cursor:
+        checklist.append(row)
+    if len(checklist) == 1:
+        return redirect("error?message=帳號已經被註冊")
     else:
-        return render_template("index.html")
+        # insert data into database
+        insert = "INSERT INTO member (name, username, password) VALUES (%s, %s, %s)"
+        insertVal = (name, username, generate_password_hash(password))
+        cursor.execute(insert, insertVal)
+        db.commit()
+        # save username into session
+        select = "SELECT id, username FROM member WHERE username=%s"
+        selectVal = (username,)
+        cursor.execute(select, selectVal)
+        for row in cursor:
+            session["userID"] = row[0]
+        # return to /member
+        return redirect("/member")
 
 # check signin data
-@app.route("/signin", methods=["GET", "POST"])
+@app.route("/signin", methods=["POST"])
 def signin():
-    # if "post"
-    if request.method == "POST":
-        # user data 
-        username = request.form.get("username")
-        password = request.form.get("password")
-        # if username or password are blank
-        if not username or not password:
-            return redirect("/error?message=請輸入帳號、密碼")
-        else:
-            # check username and password
-            check = "SELECT password FROM member WHERE username = %s"
-            checkVal = (username,)
-            cursor.execute(check, checkVal)
-            for member in cursor:
-                if check_password_hash(member[0], password):
-                    session["userID"]=username
-                    return redirect("/member")
-            # if wrong return to error
-            return redirect("/error?message=帳號或密碼輸入錯誤")
+    # user data 
+    username = request.form.get("username")
+    password = request.form.get("password")
+    # if username or password are blank
+    if not username or not password:
+        return redirect("/error?message=請輸入帳號、密碼")
     else:
-        return render_template("index.html")
+        # check username and password
+        check = "SELECT password, id FROM member WHERE username = %s"
+        checkVal = (username,)
+        cursor.execute(check, checkVal)
+        for member in cursor:
+            if check_password_hash(member[0], password):
+                session["userID"]=member[1]
+                return redirect("/member")
+        # if wrong return to error
+        return redirect("/error?message=帳號或密碼輸入錯誤")
 
 # signout and clean session and return to index
 @app.route("/signout")
@@ -99,13 +93,12 @@ def signout():
     return redirect("/")
 
 # member page
-
-@app.route("/member", methods=["GET", "POST"])
+@app.route("/member")
 def member(): 
     # if already login
     if session.get("userID"):
         # show name on /member
-        checkName = "SELECT name FROM member WHERE username=%s"
+        checkName = "SELECT name FROM member WHERE id=%s"
         checkVal = (session["userID"],)
         cursor.execute(checkName, checkVal)
         for name in cursor:
@@ -124,33 +117,21 @@ def member():
     else:
         return redirect("/")
 
-@app.route("/message", methods=["GET", "POST"])
+@app.route("/message", methods=["POST"])
 def message():
-    if request.method == "POST":
-        # get message content
-        message = request.form.get("message")
-        # get user's member_id
-        checkId = "SELECT id FROM member WHERE username=%s"
-        checkVal = (session["userID"],)
-        cursor.execute(checkId, checkVal)
-        for id in cursor:
-            member_id = id
-        # insert information into table of message
-        now = datetime.now()
-        date_now = now.strftime("%y-%m-%d %H:%M:%S")
-        insert = "INSERT INTO message (member_id, content, time) VALUES (%s, %s, %s)"
-        insertVal = (member_id[0], message, date_now)
-        cursor.execute(insert, insertVal)
-        db.commit()
-        return redirect("/member")
-    else:
-        return redirect("/member")
-
+    # get message content
+    message = request.form.get("message")
+    # insert information into table of message
+    insert = "INSERT INTO message (member_id, content) VALUES (%s, %s)"
+    insertVal = (session["userID"], message)
+    cursor.execute(insert, insertVal)
+    db.commit()
+    return redirect("/member")
 
 # if something wrong
 @app.route("/error")
 def error():
-    # show error messange
+    # show error message
     message = request.args.get("message")
     return render_template("error.html", message=message)
 
